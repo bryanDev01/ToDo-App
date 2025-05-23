421aa9d6f16ad6bac24dbbf0c5adac36138aa0e4
Brayannn <elkomikoh@gmail>
2025-05-23 15:56:10 -0400

    Fix de la searchBar

--- services/services.ts
+++ services/services.ts
@@ -5,6 +5,6 @@ export async function loadTasks(term?: string | undefined) {
   const tasks: Task[] = await prisma.task.findMany();
   if(term === undefined) return tasks
   else {
-    return tasks.filter(task => task.title.toLowerCase().startsWith(term))
+    return tasks.filter(task => task.title.toLowerCase().startsWith(term.toLowerCase()))
   }
 }
