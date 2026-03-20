7a1f43bc57f41084af4a07734c0a904138498252
Brayannn <elkomikoh@gmail>
2026-03-20 14:51:41 -0400

    fix prisma issues 3

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,6 +1,13 @@
 // prisma.config.ts
-import "dotenv/config";
+import "dotenv/config"; 
+import { defineConfig, env } from "prisma/config";
 
-export default {
+export default defineConfig({
   schema: "prisma/schema.prisma",
-};
+  migrations: {
+    path: "prisma/migrations",
+  },
+  datasource: {
+    url: env("DATABASE_URL"),
+  },
+});
